99a2c27f213babfa07865fbc1ee63f6c3b5d5ac8
gramsower <110431307+gramsower@users.noreply.github.com>
2022-10-23 14:30:37 -0700

    add API call in ipAPI.js file, begin work on HTML

---
+++ src/ipAPI.js
@@ -0,0 +1,16 @@
+export default class IPLocate {
+  static async getLocation() {
+    try {
+      const response = await fetch(`https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.API_KEY}`);
+      const jsonifiedResponse = await response.json();
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText} ${jsonifiedResponse.message}`;
+        throw new Error (errorMessage);
+      }
+      console.log(response);
+      return jsonifiedResponse;
+    } catch(error) {
+      return error;
+    }
+  }
+}
