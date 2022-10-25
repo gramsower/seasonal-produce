e991586b674ebc87f09103458c1f2c232a5f7063
Jonathan Liera <jonliera1@gmail.com>
2022-10-24 20:25:28 -0700

    add array NZ

--- src/produceLIBNZ.js
+++ src/produceLIBNZ.js
@@ -12,6 +12,8 @@ let olive = new VegetablesNZ('olive', '', 'Olives come in a variety of colors an
 let raddish = new VegetablesNZ('raddish', '', 'Most radishes are sold with the tops attached: look for vibrant greens and firm roots. Do not forget you can saute the tops or use them in pesto!');
 let tomato = new VegetablesNZ('tomato', '', 'Tomatoes come in a dizzying array of colors and shapes, so rather than relying on color alone, look for tomatoes that are heavy and fragrant. Some scarring is normal on heirloom varieties, but avoid tomatoes with freshly torn skin.');
 
+let VegArrNZ = [nopal, onion, olive, raddish, tomato];
+
 export class FruitsNZ {
   constructor (fruitName, fruitSeason =[], fruitInfo) {
     this.fruitName = fruitName;
@@ -26,6 +28,7 @@ let watermelon = new FruitsNZ('watermelon','','When choosing a watermelon, look 
 let orange = new FruitsNZ('orange','','Oranges that are still green and too hard are not yet ripe. Instead, choose an orange that has medium firmness and feels juicy when lightly squeezed.')
 let pineapple = new FruitsNZ('pineapple','','To check for ripeness, inspect the base of a pineapple and take a big whiff. Its perfume should be sweet, not fermented. Pass on any fruit that has moldy spots or leaves that are dried out.');
 
+let fruitArrNZ = [nectarine, strawberry, watermelon, orange, pineapple];
 
 export class LegumesNZ {
   constructor (legumeName, legumeSeason =[], legumeInfo) {
@@ -39,3 +42,5 @@ let peas = new LegumesNZ('peas', '', 'Peas are a type of legume and come in a va
 let snapPeas = new LegumesNZ('snappeas', '', 'Snap peas are excellent raw or cooked. When shopping, look for those with full pods that are vibrant in color, avoiding any with mushy spots.');
 let peanuts = new LegumesNZ('peanuts', '', 'Peanuts can often be purchased in their distinctive, textured shell either dry roasted or roasted in oil. Be sure to store peanuts in a cool, dry place and discard any peanuts that show signs of mold.');
 let snowPeas = new LegumesNZ('snowpeas', '', 'Good snow peas will be bright green with thin, flexible pods. Snow peas are mildly flavored and can be served raw or cooked.');
+
+let legumeArrNZ = [shellBean, peas, snapPeas, peanuts, snowPeas];
