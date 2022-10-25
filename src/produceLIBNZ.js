3eb62b5588a7a77cb46e7a9f0be11caea6f2dd05
Jonathan Liera <jonliera1@gmail.com>
2022-10-24 20:29:59 -0700

    add array into constructor function

--- src/produceLIBNZ.js
+++ src/produceLIBNZ.js
@@ -3,6 +3,7 @@ export class VegetablesNZ {
     this.vegName = vegName;
     this.vegSeason = vegSeason;
     this.vegInfo = vegInfo;
+    this.VegArrNZ = [nopal, onion, olive, raddish, tomato];
   }
 }
 
@@ -12,13 +13,14 @@ let olive = new VegetablesNZ('olive', '', 'Olives come in a variety of colors an
 let raddish = new VegetablesNZ('raddish', '', 'Most radishes are sold with the tops attached: look for vibrant greens and firm roots. Do not forget you can saute the tops or use them in pesto!');
 let tomato = new VegetablesNZ('tomato', '', 'Tomatoes come in a dizzying array of colors and shapes, so rather than relying on color alone, look for tomatoes that are heavy and fragrant. Some scarring is normal on heirloom varieties, but avoid tomatoes with freshly torn skin.');
 
-let VegArrNZ = [nopal, onion, olive, raddish, tomato];
+// let VegArrNZ = [nopal, onion, olive, raddish, tomato];
 
 export class FruitsNZ {
   constructor (fruitName, fruitSeason =[], fruitInfo) {
     this.fruitName = fruitName;
     this.fruitSeason = fruitSeason;
     this.fruitInfo = fruitInfo;
+    this.fruitArrNZ = [nectarine, strawberry, watermelon, orange, pineapple];
   }
 }
 
@@ -28,13 +30,14 @@ let watermelon = new FruitsNZ('watermelon','','When choosing a watermelon, look 
 let orange = new FruitsNZ('orange','','Oranges that are still green and too hard are not yet ripe. Instead, choose an orange that has medium firmness and feels juicy when lightly squeezed.')
 let pineapple = new FruitsNZ('pineapple','','To check for ripeness, inspect the base of a pineapple and take a big whiff. Its perfume should be sweet, not fermented. Pass on any fruit that has moldy spots or leaves that are dried out.');
 
-let fruitArrNZ = [nectarine, strawberry, watermelon, orange, pineapple];
+// let fruitArrNZ = [nectarine, strawberry, watermelon, orange, pineapple];
 
 export class LegumesNZ {
   constructor (legumeName, legumeSeason =[], legumeInfo) {
     this.legumeName = legumeName;
     this.legumeSeason = legumeSeason;
     this.legumeInfo = legumeInfo;
+    this.legumeArrNZ = [shellBean, peas, snapPeas, peanuts, snowPeas];
   }
 }
 let shellBean = new LegumesNZ('shellbean', '', 'Fresh shell beans cook much quicker than their dried out counterparts. Look for pods that have pronounced, evenly sized bumps and avoid those with noticeable empty spots.');
@@ -43,4 +46,4 @@ let snapPeas = new LegumesNZ('snappeas', '', 'Snap peas are excellent raw or coo
 let peanuts = new LegumesNZ('peanuts', '', 'Peanuts can often be purchased in their distinctive, textured shell either dry roasted or roasted in oil. Be sure to store peanuts in a cool, dry place and discard any peanuts that show signs of mold.');
 let snowPeas = new LegumesNZ('snowpeas', '', 'Good snow peas will be bright green with thin, flexible pods. Snow peas are mildly flavored and can be served raw or cooked.');
 
-let legumeArrNZ = [shellBean, peas, snapPeas, peanuts, snowPeas];
+// let legumeArrNZ = [shellBean, peas, snapPeas, peanuts, snowPeas];
