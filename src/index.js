7148f74a01e6911c377f81f63fda1b0859776bbb
Grace Lee <gracexsnoopy@gmail.com>
2022-10-24 18:30:10 -0700

    Start UI logic to display ip location

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import [] from './js/[].js';
+import IPLocate from './ipAPI';
+
+async function getLocation() {
+  const response = await IPLocate.getLocation();
+  if (response.main) {
+    printElements(response);
+  } else {
+    printError(response);
+  }
+}
+
+function printElements(response) {
+  //`${reponse}`;
+}
+
+function printError(error) {
+  //`${error}`;
+}
+
+function handle() {
+  getLocation();
+}
+
+window.addEventListener('load', function() {
+  handle();
+})
