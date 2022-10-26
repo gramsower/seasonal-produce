ab2d66c55faedd7f7c0bd613135d7327a9426baa
pphila <efl2ice89@gmail.com>
2022-10-25 20:05:27 -0700

    WIP for accessing produce library
    Co-authored-by: Grace Lee <gracexsnoopy@gmail.com>
    Co-authored-by: Jonathan Liera <jonliera1@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { vegArrObjectAM } from './js/produceLibAM';
+import { getVegetables } from './js/produceLibAM';
+//import { VegetablesNZ, FruitsNZ, LegumesNZ } from './js/produceLIBNZ';
 import IPLocate from './ipAPI';
 
 async function getLocation() {
@@ -20,3 +23,21 @@ window.addEventListener('load', function() {
   handle();
 });
 
+vegArrObjectAM.map;
+
+
+// function getProduce() {
+//   for (let i=0; i < vegArrAM.length; i++) {
+//     const vegetable = new VegetablesAM.vegArrAM[i];
+//     console.log(vegetable.vegArrAM)
+//     produceArray.push(vegetable);
+//     console.log(vegetable);
+//   }
+// }
+// getProduce();
+
+// const displayProduce = (vegetable) => {
+//   const displayDiv = document.querySelector('#card-content');
+//   const cardHTMLString = vegArr.map ( vegetable => `
+//   <`)
+// }
