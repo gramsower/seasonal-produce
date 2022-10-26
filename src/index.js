ea6f9f43a88d68785114b3c2caab399167aad171
gramsower <110431307+gramsower@users.noreply.github.com>
2022-10-25 20:11:20 -0700

    clean up function to obtain user location from API and add basic function to determine user region

--- src/index.js
+++ src/index.js
@@ -3,12 +3,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import IPLocate from './ipAPI';
 
-async function getLocation() {
+async function getLocation(response) {
   const response = await IPLocate.getLocation();
-  printElements(response);
+  let userState = response.state_prov;
+  determineUserRegion(userState);
 }
 
-function printElements(response) {
+function determineUserRegion(userState) {
+  // let userState = response.state_prov;
+  if (userState === ('washington' || 'oregon' || 'idaho' || 'wyoming' || 'montana')) {
+    // return produce <-- based on northwest region
+  } else if (userState === ('texas' || 'nevada' || 'arizona' || 'colorado' || 'california' || 'utah' || 'new mexico' || 'oklahoma')) {
+   // return produce <-- based on southwest region
+  } else if (userState === ('north dakota' || 'south dakota' || 'nebraska' || 'kansas' || 'minnesota' || 'iowa' || 'indiana' || 'michigan' || 'wisconsin' || 'illinois' || 'ohio' || 'kentucky' || 'missouri' )) {
+    //return produce <-- based on midwest region
+  } else if (userState === ('west virginia' || 'virginia' || 'north carolina' || 'south carolina' || 'tennessee' || 'mississippi' || 'alabama' || 'georgia' || 'florida' || 'arkansas')) {
+    // return produce <--based on south region
+  } else (userState === ('maine' || 'new hampshire' || 'vermont' || 'new york' || 'massachussetts' || 'connecticut' || 'rhode island' || 'new jersey' || 'pennsylvania' || 'maryland' || 'delaware')) {
+    // return produce <-- based on northeast region
+  }
+}
+
+function printElements(location) {
   console.log(response.state_prov);
 }
 
@@ -17,5 +33,5 @@ function handle() {
 }
 
 window.addEventListener('load', function() {
-  handle();
+  document.querySelector('form').addEventListener('submit', handle());
 });
