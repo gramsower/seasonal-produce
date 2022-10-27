af09381a1accb2350af2cabd7de46515af8d0ba4
gramsower <110431307+gramsower@users.noreply.github.com>
2022-10-26 20:46:54 -0700

    finalize determineUserRegion function to return user region, WIP function to determine a user's season based upon API call

--- src/index.js
+++ src/index.js
@@ -4,34 +4,54 @@ import './css/styles.css';
 import { vegArrObjectAM } from './js/produceLibAM';
 //import { getVegetables } from './js/produceLibAM';
 //import { VegetablesNZ, FruitsNZ, LegumesNZ } from './js/produceLIBNZ';
-import IPLocate from './ipAPI';
+import IPLocate from './js/ipAPI';
 
-async function getLocation(response) {
+
+async function getLocation() {
   const response = await IPLocate.getLocation();
   let userState = response.state_prov;
+  let dateReturned = response.time_zone.current_time;
+  let date = dateReturned.slice(5, 10);
   determineUserRegion(userState);
+  determineUserSeason(date);
 }
 
 function determineUserRegion(userState) {
   if (userState === ('washington' || 'oregon' || 'idaho' || 'wyoming' || 'montana')) {
-    // add code to alert user to their region
-    // return produce <-- based on northwest region
+    return 'northwest';
   } else if (userState === ('texas' || 'nevada' || 'arizona' || 'colorado' || 'california' || 'utah' || 'new mexico' || 'oklahoma')) {
-   // return produce <-- based on southwest region
-  } else if (userState === ('north dakota' || 'south dakota' || 'nebraska' || 'kansas' || 'minnesota' || 'iowa' || 'indiana' || 'michigan' || 'wisconsin' || 'illinois' || 'ohio' || 'kentucky' || 'missouri' )) {
-    //return produce <-- based on midwest region
+    return 'southwest';
+  } else if (userState === ('north dakota' || 'south dakota' || 'nebraska' || 'kansas' || 'minnesota' || 'iowa' || 'indiana' || 'michigan' || 'wisconsin' || 'illinois' || 'ohio' || 'kentucky' || 'missouri')) {
+    return 'midwest';
   } else if (userState === ('west virginia' || 'virginia' || 'north carolina' || 'south carolina' || 'tennessee' || 'mississippi' || 'alabama' || 'georgia' || 'florida' || 'arkansas')) {
-    // return produce <--based on south region
+    return 'south';
   } else if (userState === ('maine' || 'new hampshire' || 'vermont' || 'new york' || 'massachussetts' || 'connecticut' || 'rhode island' || 'new jersey' || 'pennsylvania' || 'maryland' || 'delaware')) {
-    // return produce <-- based on northeast region
+    return 'northeast';
   } else {
-    return error;
+    return 'error, no region available';
   }
 }
 
-function printElements() {
-  console.log(response.state_prov);
+function determineUserSeason(date) {
+  let month = date.slice(0,2);
+  let day = date.slice(3, 5);
+ // console.log(day);
+  if (month === ('10' || '11')) {
+    return 'fall';
+  } else if (month == ('1' || '2')) {
+    return'winter';
+  } else if (month == ('4' || '5')) {
+    return 'spring';
+  } else if (month == ('7' || '8')) {
+    return 'summer';
+  } else {
+    return 'error retreiving season';
+  }
 }
+
+//function printElements() {
+//   console.log(response.state_prov);
+// }
 
 function handle() {
   getLocation();
@@ -39,7 +59,7 @@ function handle() {
 
 window.addEventListener('load', function() {
   document.querySelector('form').addEventListener('submit', handle());
-})
+});
 
 
 
@@ -53,7 +73,6 @@ window.addEventListener('load', function() {
 // }
 // getProduce();
 const vegArrayInner = vegArrObjectAM;
-console.log(vegArrayInner)
 
 const displayProduce = (vegArrayInner) => {
   const displayDiv = document.querySelector('#card-content');
@@ -65,6 +84,6 @@ const displayProduce = (vegArrayInner) => {
   </li>
   `).join('');
   displayDiv.innerHTML = cardHTMLString;
-}
+};
 
 displayProduce(vegArrayInner);
