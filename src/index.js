8ff30ab47bbf2c6f29aaa170d6ca3695e369f650
Grace Lee <gracexsnoopy@gmail.com>
2022-10-30 11:52:46 -0700

    WIP: connect functions to ultimately display produce by season and region

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { produceArrObject } from './js/produceLib';
 import IPLocate from './ipAPI';
-import Recipe from './recipeAPI';
 
 async function getLocation() {
   const response = await IPLocate.getLocation();
@@ -11,12 +10,7 @@ async function getLocation() {
   let dateReturned = response.time_zone.current_time;
   let date = dateReturned.slice(5, 10);
   determineUserRegion(userState);
-}
-async function getRecipe(searchInput) {
-  const response = await Recipe.getRecipe(searchInput);
-  return response;
-  //need event listener that display recipes for a specific produce which would be what the searchInput parameter becomes
-  //need function to display recipe options
+  determineUserSeason(date);
 }
 
 function determineUserRegion(userState) {
@@ -31,41 +25,49 @@ function determineUserRegion(userState) {
   } else if (userState === ('maine' || 'new hampshire' || 'vermont' || 'new york' || 'massachussetts' || 'connecticut' || 'rhode island' || 'new jersey' || 'pennsylvania' || 'maryland' || 'delaware')) {
     return 'northeast';
   } else {
-    return ''Your location could not be determined', no region available';
+    return 'Your location could not be determined. No region available';
   }
 }
 
 function determineUserSeason(date) {
-  let month = date.slice(0,2);
+  let month = date.slice(0, 2);
   let day = date.slice(3, 5);
  // console.log(day);
-  if (month === ('10' || '11')) {
-    return 'fall';
+  if (month == ('10' || '11')) {
+    return 'fall'; 
   } else if (month == ('1' || '2')) {
-    return'winter';
+    return 'winter'; 
   } else if (month == ('4' || '5')) {
-    return 'spring';
+    return 'spring'; 
   } else if (month == ('7' || '8')) {
-    return 'summer';
+    return 'summer'; 
   } else {
     return 'error retreiving season';
   }
 }
 
-//function printElements() {
-//   console.log(response.state_prov);
+//function determineProduce(determineUserRegion, determineUserSeason) {
+// let produceArray = [];
+// produceArray.push(produce);
 // }
+// ??? think Jonathan was working on using switch cases that determines only the seasonal availability of produces and not region based availability
 
-function handle() {
-  getLocation();
-}
-
-window.addEventListener('load', function() {
-  document.querySelector('form').addEventListener('submit', handle());
-});
 
 const prodArrToDisp = produceArrObject;
 console.log(prodArrToDisp);
+
+//function produceDisplay(determineProduce) { **not final function just a general idea**
+// let produceArray = [];
+// 
+// for (let i = 0; i < prodArrToDisp.length; i++) {
+//  for (let j = 0; j < determineProduce.length; j++) {
+//   if (prodArrToDisp[i] === determineProduce[j]) {
+//      produceArray.push(prodArrToDisp[i]);
+//   }      
+//  }
+// }
+//}
+//function to compare produce we need to display (determined by region and season), put this result into displayProduce()
 
 const displayProduce = (prodArrToDisp) => {
   const displayDiv = document.querySelector('#card-content');
@@ -80,3 +82,21 @@ const displayProduce = (prodArrToDisp) => {
 };
 
 displayProduce(prodArrToDisp);
+
+function handle() {
+  getLocation();
+}
+
+window.addEventListener('load', function() {
+  handle();
+});
+
+// when browser loads it gets user's location and date (based on IP address) 
+// then location is passed into a function that determines user's region 
+// the date is passed into a function that determines season 
+// both season and region are passed into a function that determines what produce are available in that season and region
+// when it determines which produces to display, it passes it into a function that displays those produce and it's details
+// then for each produce that's displayed
+// STRETCH GOALS WITH RECIPE API
+// it will make api recipe calls for each produce that is displayed
+// it will display the recipes for each produce
